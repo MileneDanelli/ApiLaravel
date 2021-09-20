1ce1b4ad7ebea77e0f8dd4f7b9a17cac4dd4bee7
Milene Danelli <mi.danelli@outlook.com>
2021-09-20 17:24:24 -0300

    inicio autenticacao com sanctum.

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriasController;
 use App\Http\Controllers\ProdutosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Categorias
-Route::resource('/categorias', CategoriasController::class);
-Route::get('/categorias/search/{name}', [CategoriasController::class, 'search']);
+//Registrar
+Route::post('/registrar', [AuthController::class, 'register']);
 
-//Produtos
-Route::resource('/produtos', ProdutosController::class);
-Route::get('/produtos/search/{name}', [ProdutosController::class, 'search']);
+//Rotas Protegidas
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    //Categorias
+    Route::resource('/categorias', CategoriasController::class);
+    Route::get('/categorias/search/{name}', [CategoriasController::class, 'search']);
+
+    //Produtos
+    Route::resource('/produtos', ProdutosController::class);
+    Route::get('/produtos/search/{name}', [ProdutosController::class, 'search']);
+
+    //Logout
+    Route::post('/sair', [AuthController::class, 'logout']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
